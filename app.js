18d8ab6c8a01bcd04b8bcfdec98508351ba6c236
HIMANSHI DAYAL <120187837+himanshidayal14@users.noreply.github.com>
2023-09-30 12:27:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,24 +17,8 @@ menuItems.forEach((item, index) => {
 
     //change the choosen product
     choosenProduct = products[index];
-
-    //change texts of currentProduct
-   /*  currentProductTitle.textContent = choosenProduct.title;
-    currentProductPrice.textContent = "$" + choosenProduct.price;
-    currentProductImg.src = choosenProduct.colors[0].img; */
-    
-    //assing new colors
-    /* currentProductColors.forEach((color,index)=>{
-        color.style.backgroundColor=choosenProduct.colors[index].code;
-    }); */
     });
 }); 
-
-/* currentProductColors.forEach((color,index)=>{
-    color.addEventListener("click",()=>{
-     currentProductImg.src=choosenProduct.colors[index].img;
-    });
-}); */
 
 currentProductSizes.forEach((size,index)=>{
     size.addEventListener("click",()=>{
